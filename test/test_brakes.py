47bb307e6be1a9aa1e11c79b9b54f9d9504513a2
d33k <thomas.deakin@outlook.com>
2023-02-23 12:50:11 +0000

    renamed variable to snake_case

--- test/test_brakes.py
+++ test/test_brakes.py
@@ -1,25 +1,25 @@
 from src.component import (Brakes)
 
 def test_brakes_initializes_with_correct__current_state_max_lifespan():
-    newbrakes = Brakes(4, 10)
+    new_brakes = Brakes(4, 10)
 
-    assert newbrakes.current_state == 4
-    assert newbrakes.max_lifespan == 10
+    assert new_brakes.current_state == 4
+    assert new_brakes.max_lifespan == 10
 
-    assert newbrakes.check_condition() == 'Poor'
+    assert new_brakes.check_condition() == 'Poor'
 
 
 def test_brakes__use_method_increments_current_state():
-    newbrakes = Brakes(3, 10)
-    result = newbrakes.use()
+    new_brakes = Brakes(3, 10)
+    result = new_brakes.use()
 
-    assert newbrakes.current_state == 5
+    assert new_brakes.current_state == 5
     assert result == True
 
 
 def test_brakes_use_method_does_not_allow_use_if_broken():
-    newbrakes = Brakes(10, 10)
-    result = newbrakes.use()
+    new_brakes = Brakes(10, 10)
+    result = new_brakes.use()
     assert result == False
 
-    assert newbrakes.current_state == 10
+    assert new_brakes.current_state == 10
