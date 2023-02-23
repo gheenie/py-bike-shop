8d14166332df25b0509cdc9901b249e8f57c2193
d33k <thomas.deakin@outlook.com>
2023-02-23 12:31:35 +0000

    solve task 2

--- src/component.py
+++ src/component.py
@@ -9,20 +9,70 @@ class Component:
 
         if percentage_lifespan <= 10:
             condition = 'Pristine'
-        
+
         if 10 < percentage_lifespan <= 30:
             condition = 'Good'
-        
+
         if 30 < percentage_lifespan <= 60:
             condition = 'Poor'
-        
+
         if 60 < percentage_lifespan < 100:
             condition = 'Fragile'
-        
+
         if percentage_lifespan >= 100:
             condition = 'Broken'
-        
-        print(f'The condition of the bike is {condition}.')
+
+        print(f'The condition of the {type(self)} is {condition}.')
 
         return condition
-    
+
+
+class Bell(Component):
+    def __init__(self, current_state, max_lifespan):
+        super().__init__(current_state, max_lifespan)
+
+    def use(self):
+
+        if self.check_condition() == 'Broken':
+            return False
+
+        self.current_state += 1
+        return True
+
+class Brake(Component):
+    def __init__(self, current_state, max_lifespan):
+        super().__init__(current_state, max_lifespan)
+
+    def use(self):
+
+        if self.check_condition() == 'Broken':
+            return False
+
+        self.current_state += 2
+        return True
+
+class Chain(Component):
+    def __init__(self, current_state, max_lifespan):
+        super().__init__(current_state, max_lifespan)
+
+    def use(self):
+
+        if self.check_condition() == 'Broken':
+            return False
+
+        self.current_state += 3
+        return True
+
+class Tyres(Component):
+    def __init__(self, current_state, max_lifespan):
+        super().__init__(current_state, max_lifespan)
+
+    def use(self):
+
+        if self.check_condition() == 'Broken':
+            return False
+
+        self.current_state += 4
+        return True
+
+
--- test/test_component.py
+++ test/test_component.py
@@ -1,10 +1,12 @@
-from src.component import (Component)
+from src.component import (Component, Bell, Brake, Chain, Tyres)
+
 
 def test_initialise_component():
     newComponent = Component(9, 10)
 
     assert newComponent.current_state == 9
     assert newComponent.max_lifespan == 10
+
 
 def test_check_condition():
     newComponent = Component(0, 10)
@@ -26,10 +28,110 @@ def test_check_condition():
 
     newComponent = Component(9, 100)
     assert newComponent.check_condition() == 'Pristine'
-    
+
     newComponent = Component(99, 100)
     assert newComponent.check_condition() == 'Fragile'
+
 
 def test_check_condition_errors():
     newComponent = Component(11, 10)
     assert newComponent.check_condition() == 'Broken'
+
+
+def test_bell_initializes_with_correct__current_state_max_lifespan():
+    newBell = Bell(4, 10)
+
+    assert newBell.current_state == 4
+    assert newBell.max_lifespan == 10
+
+    assert newBell.check_condition() == 'Poor'
+
+
+def test_bell__use_method_increments_current_state():
+    newBell = Bell(3, 10)
+    result = newBell.use()
+
+    assert newBell.current_state == 4
+    assert result == True
+
+
+def test_bell_use_method_does_not_allow_use_if_broken():
+    newBell = Bell(10, 10)
+    result = newBell.use()
+    assert result == False
+
+    assert newBell.current_state == 10
+
+
+def test_brakes_initializes_with_correct__current_state_max_lifespan():
+    newbrake = Brake(4, 10)
+
+    assert newbrake.current_state == 4
+    assert newbrake.max_lifespan == 10
+
+    assert newbrake.check_condition() == 'Poor'
+
+
+def test_brakes__use_method_increments_current_state():
+    newbrake = Brake(3, 10)
+    result = newbrake.use()
+
+    assert newbrake.current_state == 5
+    assert result == True
+
+
+def test_brakes_use_method_does_not_allow_use_if_broken():
+    newbrake = Brake(10, 10)
+    result = newbrake.use()
+    assert result == False
+
+    assert newbrake.current_state == 10
+
+def test_chain_initializes_with_correct__current_state_max_lifespan():
+    new_chain = Chain(15, 20)
+
+    assert new_chain.current_state == 15
+    assert new_chain.max_lifespan == 20
+
+    assert new_chain.check_condition() == 'Fragile'
+
+
+def test_chain__use_method_increments_current_state():
+    new_chain = Chain(3, 10)
+    result = new_chain.use()
+
+    assert new_chain.current_state == 6
+    assert result == True
+
+
+def test_chain_use_method_does_not_allow_use_if_broken():
+    new_chain = Chain(10, 10)
+    result = new_chain.use()
+    assert result == False
+
+    assert new_chain.current_state == 10
+
+def test_tyres_initializes_with_correct__current_state_max_lifespan():
+    new_tyres = Tyres(15, 20)
+
+    assert new_tyres.current_state == 15
+    assert new_tyres.max_lifespan == 20
+
+    assert new_tyres.check_condition() == 'Fragile'
+
+
+def test_tyres__use_method_increments_current_state():
+    new_tyres = Tyres(3, 10)
+    result = new_tyres.use()
+
+    assert new_tyres.current_state == 7
+    assert result == True
+
+
+def test_tyres_use_method_does_not_allow_use_if_broken():
+    new_tyres = Tyres(10, 10)
+    result = new_tyres.use()
+    assert result == False
+
+    assert new_tyres.current_state == 10
+
