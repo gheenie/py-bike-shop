054bfadf5391dac575e993a18943fc47417760bc
d33k <thomas.deakin@outlook.com>
2023-02-23 12:47:44 +0000

    renamed variables to snake_case

--- test/test_bell.py
+++ test/test_bell.py
@@ -1,25 +1,25 @@
 from src.component import (Bell)
 
 def test_bell_initializes_with_correct__current_state_max_lifespan():
-    newBell = Bell(4, 10)
+    new_bell = Bell(4, 10)
 
-    assert newBell.current_state == 4
-    assert newBell.max_lifespan == 10
+    assert new_bell.current_state == 4
+    assert new_bell.max_lifespan == 10
 
-    assert newBell.check_condition() == 'Poor'
+    assert new_bell.check_condition() == 'Poor'
 
 
 def test_bell__use_method_increments_current_state():
-    newBell = Bell(3, 10)
-    result = newBell.use()
+    new_bell = Bell(3, 10)
+    result = new_bell.use()
 
-    assert newBell.current_state == 4
+    assert new_bell.current_state == 4
     assert result == True
 
 
 def test_bell_use_method_does_not_allow_use_if_broken():
-    newBell = Bell(10, 10)
-    result = newBell.use()
+    new_bell = Bell(10, 10)
+    result = new_bell.use()
     assert result == False
 
-    assert newBell.current_state == 10
+    assert new_bell.current_state == 10
