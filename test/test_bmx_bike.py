dfe9755393ea67aa4ad2474dd9363aed64632365
infoomics <sbx_94@hotmail.com>
2023-02-24 19:43:04 +0000

    remove unneeded asserts for initialisation

--- test/test_bmx_bike.py
+++ test/test_bmx_bike.py
@@ -1,34 +1,28 @@
-from src.component import (Bell, Brakes, Chain, Tyres)
+from src.component import (Bell, Chain, Tyres)
 from src.bikes import (BMXBike)
 
 
 def test_tyres_wear_15_percent_more_per_ride():
-
     new_bell = Bell(0, 10)
     new_chain = Chain(0, 30)
     new_tyres = Tyres(0, 40)
-
     new_bmx_bike = BMXBike(
         new_bell, new_chain, new_tyres)
 
-    assert new_bmx_bike.components['tyres'].current_state == 0
-
     new_bmx_bike.ride()
+
     assert new_bmx_bike.components['tyres'].current_state == 4.6
 
     new_bmx_bike.ride()
+
     assert new_bmx_bike.components['tyres'].current_state == 9.2
 
+
 def test_no_brake_component():
-
-
     new_bell = Bell(0, 10)
     new_chain = Chain(0, 30)
     new_tyres = Tyres(0, 40)
-
     new_bmx_bike = BMXBike(
         new_bell, new_chain, new_tyres)
 
     assert new_bmx_bike.components.get('brakes') == None
-
-
--- test/test_mountain_bike.py
+++ test/test_mountain_bike.py
@@ -3,19 +3,17 @@ from src.bikes import (MountainBike)
 
 
 def test_has_a_15_percent_stronger_chain():
-
     new_bell = Bell(0, 10)
     new_brakes = Brakes(0, 20)
     new_chain = Chain(0, 30)
     new_tyres = Tyres(0, 40)
-
     new_mountain_bike = MountainBike(
         new_bell, new_brakes, new_chain, new_tyres)
 
-    assert new_mountain_bike.components['chain'].current_state == 0
-
     new_mountain_bike.ride()
+
     assert new_mountain_bike.components['chain'].current_state == 2.55
 
     new_mountain_bike.ride()
+    
     assert new_mountain_bike.components['chain'].current_state == 5.1
