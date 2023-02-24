3dd2d59b30197bfee438a34c0e8a79020d082bdb
infoomics <sbx_94@hotmail.com>
2023-02-24 12:49:51 +0000

    fill out method bodies for task 4

--- src/service_person.py
+++ src/service_person.py
@@ -1,16 +1,31 @@
+from src.component import (Bell, Brakes, Chain, Tyres)
+
 class ServicePerson:
 
     def __init__(self, bike):
         self.current_bike = bike
 
     def order_parts(self):
-        pass
+        fresh_parts = {
+            'bell': Bell(0, 10),
+            'brakes': Brakes(0, 20),
+            'chain': Chain(0, 30),
+            'tyres': Tyres(0, 40)
+        }
+
+        for key, component in self.current_bike.components.items():
+            if component.check_condition() == 'Broken':
+                self.current_bike.components[key] = fresh_parts[key]
 
     def service_parts(self):
-        pass
+        for component in self.current_bike.components.values():
+            if component.check_condition() in ['Fragile', 'Poor']:
+                component.set_current_state('Good')
 
     def oil(self):
-        pass
+        for key, component in self.current_bike.components.items():
+            if component.check_condition() == 'Good' and key.capitalize() in ['Chain', 'Bell', 'Brakes']:
+                component.set_current_state('Pristine')
 
     def pump_wheels(self):
         pass
