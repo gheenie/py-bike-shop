6d0350866cb6dff601796fdd660285f1daef8d9d
infoomics <sbx_94@hotmail.com>
2023-02-24 19:38:11 +0000

    group asserts by expected; current_state > max_lifespan no longer considered an error

--- test/test_component.py
+++ test/test_component.py
@@ -13,6 +13,8 @@ def test_check_condition():
     assert newComponent.check_condition() == 'Pristine'
     newComponent = Component(1, 10)
     assert newComponent.check_condition() == 'Pristine'
+    newComponent = Component(9, 100)
+    assert newComponent.check_condition() == 'Pristine'
 
     newComponent = Component(3, 10)
     assert newComponent.check_condition() == 'Good'
@@ -22,25 +24,10 @@ def test_check_condition():
 
     newComponent = Component(9, 10)
     assert newComponent.check_condition() == 'Fragile'
-
-    newComponent = Component(10, 10)
-    assert newComponent.check_condition() == 'Broken'
-
-    newComponent = Component(9, 100)
-    assert newComponent.check_condition() == 'Pristine'
-
     newComponent = Component(99, 100)
     assert newComponent.check_condition() == 'Fragile'
 
-
-def test_check_condition_errors():
+    newComponent = Component(10, 10)
+    assert newComponent.check_condition() == 'Broken'
     newComponent = Component(11, 10)
     assert newComponent.check_condition() == 'Broken'
-
-
-
-
-
-
-
-
