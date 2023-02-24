d0088a77678b53289c7905f60435a74c77f5e263
infoomics <sbx_94@hotmail.com>
2023-02-24 12:50:58 +0000

    add spacing for PEP8

--- src/service_person.py
+++ src/service_person.py
@@ -1,9 +1,10 @@
 from src.component import (Bell, Brakes, Chain, Tyres)
 
-class ServicePerson:
 
+class ServicePerson:
     def __init__(self, bike):
         self.current_bike = bike
+
 
     def order_parts(self):
         fresh_parts = {
@@ -17,26 +18,30 @@ class ServicePerson:
             if component.check_condition() == 'Broken':
                 self.current_bike.components[key] = fresh_parts[key]
 
+
     def service_parts(self):
         for component in self.current_bike.components.values():
             if component.check_condition() in ['Fragile', 'Poor']:
                 component.set_current_state('Good')
+
 
     def oil(self):
         for key, component in self.current_bike.components.items():
             if component.check_condition() == 'Good' and key.capitalize() in ['Chain', 'Bell', 'Brakes']:
                 component.set_current_state('Pristine')
 
+
     def pump_wheels(self):
         pass
+
 
     def service_bike(self):
         pass
 
+
     def check_safety(self):
         pass
 
+
     def check_up(self):
         pass
-
-
